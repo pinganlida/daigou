1180c7871d8b21122bd2eaf8bdbb0e7abb7f1152
violetelegy <zhenyu.zhou.pku@gmail.com>
2013-04-29 21:59:36 +1000

    register-pseudo code

--- register.php
+++ register.php
@@ -4,10 +4,23 @@ require('./libs/Smarty.class.php');
 $smarty = new Smarty;
 
 include_once './include/include.php';
-// //$smarty->force_compile = true;
-// $smarty->debugging = true;
-// $smarty->caching = true;
-// $smarty->cache_lifetime = 120;
+
+
+if($_POST["submit"]==null){
+	//未提交的页面， 显示输入用户名，密码，电子邮件	
+}else{
+	//提交后的页面
+	
+	//TODO 判定输入
+	if(success){
+		//如果成功，输出一个提示页面（general的提示页面)
+	}
+	else{
+		//如果失败,则返回输入页面，并且，提示出错的地方
+		//把出错信息放到一个数组里面输出到页面，提示给用户
+	}	
+	
+}
 
 $smarty->assign("Name","wake me up",true);
 
@@ -15,10 +28,31 @@ for($i=0;$i<20;$i++){
 	$list[$i] =  "hahahaha-".$i;
 }
 
+
+
+
+
 $smarty->assign("list",$list);
 
 $smarty->assign("header2",$smarty->fetch("header2.tpl"));
 
 
 $smarty->display('index.tpl');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
--- templates/index.tpl
+++ templates/index.tpl
@@ -1,6 +1,7 @@
 {config_load file="test.conf" section="setup"}
 {include file="header.tpl" title=foo}
 
+{$header2}
 代购平台 header
 <hr>
 首页<br>
@@ -8,12 +9,24 @@ helloword
 <hr>
 {$Name}
 <hr>
-
+<table border=1>
 {section name=outer loop=$list}
-	{$list[outer]}<br>
+	<tr>
+		<td>
+		{$list[outer]}<br>
+		</td>
+		<td>
+			rambo
+		</td>
+	</tr>
+	
 {sectionelse}
 	none
 {/section}
+</table>
+
+
+
 
 <hr>
 footer

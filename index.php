0cac9f174192bcaf9f381797ee04264a255d947f
zhangruihe <zhangruiheob@gmail.com>
2013-04-30 22:37:06 +1000

    overwrite local

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ include_once './include/include.php';
 $smarty->assign("Name","wake me up",true);
 
 for($i=0;$i<20;$i++){
-	$list[$i] =  "hahahahahahahahahaha-".$i;
+	$list[$i] =  "hohohohohohoho-".$i;
 }
 
 

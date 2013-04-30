0898484e4538d0ffd21765ec1500917bcb8a33a9
zhangruihe <zhangruiheob@gmail.com>
2013-04-30 18:02:48 +1000

    register
    still have some bugs need to fix.error message cannot display.but logic
    is right

--- register.php
+++ register.php
@@ -5,54 +5,74 @@ $smarty = new Smarty;
 
 include_once './include/include.php';
 
+$smarty->assign("Name","注册",true);
 
-if($_POST["submit"]==null){
-	//未提交的页面， 显示输入用户名，密码，电子邮件	
-}else{
+if(isset($_POST['submit']))
+{ 
 	//提交后的页面
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$repassword = $_POST['repass'];
+	$email = $_POST['email'];
 	
-	//TODO 判定输入
-	if(success){
+	if(empty($username))
+	{
+		$error .= '用户名不能为空<br>';
+	}
+	if(empty($password))
+	{
+		$error .= '密码不能为空<br>';
+	}
+	if(empty($repassword))
+	{
+		$error .= '再次输入密码不能为空<br>';
+	}
+	if(empty($email))
+	{
+		$error .= '邮箱不能为空<br>';
+	}
+	if(strlen($password) < 6)
+	{
+		$error .= '密码长度不能小于6位<br>';
+	}
+	if($_POST['password']!=$_POST['re_password'])
+	{
+		$error .= '2次密码不一样<br>';
+	}
+/*	if(!preg_match('/^[\w\x80-\xff]{3,15}$/', $username))
+	{
+		$error .= '用户名不匹配<br>';
+	}
+	if(!preg_match('/^w+([-+.]w+)*@w+([-.]w+)*.w+([-.]w+)*$/', $email))
+	{
+		$error .= '邮箱不匹配<br>';
+	} */
+}	
+//未提交的页面， 显示输入用户名，密码，电子邮件
+else
+{
+	
+}
+	/*if(success)
+	{
 		//如果成功，输出一个提示页面（general的提示页面)
 	}
-	else{
+	else
+	{
 		//如果失败,则返回输入页面，并且，提示出错的地方
 		//把出错信息放到一个数组里面输出到页面，提示给用户
-	}	
+	}	*/
 	
+
+
+//$smarty->assign("header2",$smarty->fetch("header2.tpl"));
+
+
+if(isset($error))
+{
+	$smarty->assign("Error",$error,true);
+	unset($error);
 }
-
-$smarty->assign("Name","wake me up",true);
-
-for($i=0;$i<20;$i++){
-	$list[$i] =  "hahahaha-".$i;
-}
-
-
-
-
-
-$smarty->assign("list",$list);
-
-$smarty->assign("header2",$smarty->fetch("header2.tpl"));
-
-
-$smarty->display('index.tpl');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$smarty->display('register.tpl');
 
 ?>
---
+++ templates/register.tpl
@@ -0,0 +1,17 @@
+{config_load file="test.conf" section="setup"}
+{include file="header.tpl" title=foo}
+
+{$header2}
+代购平台 header
+<hr>
+{$Name}
+<hr>
+<form name="RegForm" action="register.php" method="post">
+输入用户名: <input id="username"  name="username" type="text" />
+设置密码: <input  id="password" name="password" type="password" />
+重复密码: <input  id="repass" name="repass" type="password" />
+电子邮箱: <input id="email" name="email" type="text" />
+<input type="submit" name="submit" value="  提交注册  " />
+<hr>
+{$Error}
+<hr>

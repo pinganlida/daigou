e6a746fac38d94264ecf5341bd7fcc290fd74a88
violetelegy <zhenyu.zhou.pku@gmail.com>
2013-05-06 21:48:39 +1000

    add faq, aboutus, contact us, change index

---
+++ contactus.php
@@ -0,0 +1,17 @@
+<?php
+require('./libs/Smarty.class.php');
+require('./include/mysql.php');
+
+
+$smarty = new Smarty;
+// $db = new mysqlconnect();
+require('./include/juglogin.php');
+include_once './include/include.php';
+// //$smarty->force_compile = true;
+// $smarty->debugging = true;
+// $smarty->caching = true;
+// $smarty->cache_lifetime = 120;
+
+$smarty->display('contactus.tpl');
+// $db->disconnect();
+?>

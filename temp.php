891620525392458df0c8a68584de6bde252da4dc
violetelegy <zhenyu.zhou.pku@gmail.com>
2013-05-12 04:16:54 +1000

    add domain name

--- temp.php
+++
@@ -1,8 +0,0 @@
-<?php
-//example of insert
-// $sql = "insert into product (name,description) values ('添加商品测试','添加商品描述测试')";
-// $newid = $db->query_insert($sql);
-//echo "newid = ".$newid;
-
-phpinfo();
-?>
